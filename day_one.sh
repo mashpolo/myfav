31970eee38144e122b7bdee79b1fac0b4de6ffae
mashpolo <luluo.polo1@gmail.com>
2018-07-26 11:45:17 +0800

    fix bugs if excute script not in base path

--- day_one.sh
+++ day_one.sh
@@ -10,8 +10,9 @@ then
    exit 0
 fi
 # copy template from base
-cp -rp ./dayone_base ./dayone_tp
-dayone_template="./dayone_tp"
+base_dir="/Users/luluo/work/github/myfav"
+cp -rp $base_dir/dayone_base $base_dir/dayone_tp
+dayone_template=$base_dir"/dayone_tp"
 # this file name will trigger vim to create a new file with given template
 vim $dayone_template
  
